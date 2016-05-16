c15316320cf4fe53d985cd892309dd0c74f9ef15
Haruka <haruka@tsundere.moe>
2016-05-16 15:47:01 +0900

    Initial commit

---
+++ SteamAuth/SteamAuth.h
@@ -0,0 +1,19 @@
+//
+//  SteamAuth.h
+//  SteamAuth
+//
+//  Created by Haruka Ma on H28/05/16.
+//  Copyright © 平成28年 Haruka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SteamAuth.
+FOUNDATION_EXPORT double SteamAuthVersionNumber;
+
+//! Project version string for SteamAuth.
+FOUNDATION_EXPORT const unsigned char SteamAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SteamAuth/PublicHeader.h>
+
+
